d7e37561eb3522229d92ad042e58cfa1cf31e1ee
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-25 09:04:45 +0100

    completed nonRepeatedChar()

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,27 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+	var characters = {};
+
+	// assiging each unique character of the string to characters object
+	for (var i = 0; i < string.length; i++) {
+		characters[string[i]] = 0;
+	}
+
+	// counting each character's occureence in the string
+	Object.keys(characters).forEach(function(element) {
+		for (var i = 0; i < string.length; i++) {
+			if (element === string[i]) {
+				characters[element] += 1;
+			}
+		}
+	});
+    //{A: 2, C: 1, B: 2, D: 1}
+    
+    // returning the first character of the occurence value of 1
+    for (key in characters){
+    	if (characters[key] === 1) {
+    		return key;
+    	}
+    }
 };
