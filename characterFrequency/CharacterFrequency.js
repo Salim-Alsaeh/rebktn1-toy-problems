9788b54b628e652cdd066d3dc8987d1dcaec416c
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-05 09:31:11 +0100

    tpLostCountofNumber completed

--- characterFrequency/CharacterFrequency.js
+++ characterFrequency/CharacterFrequency.js
@@ -11,3 +11,48 @@ characterFrequency("mississippi")  -->  [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ 
 
 characterFrequency("") -->  [ ]
 */
+
+function characterFrequency(string) {
+	var charFreqs = {};
+	// counting frequencies
+	for (var i = 0; i < string.length; i++) {
+		var count = 0;
+		for (var j = 0; j < string.length; j++) {
+			if (string[i] === string[j]) {
+				count++;
+			}
+		}
+		charFreqs[string[i]] = count;
+	}
+
+	var charFreqsArray = Object.entries(charFreqs);
+
+	//sortiing in ascending order by charachter 
+	charFreqsArray.sort(function(a, b){
+		var charA = a[0].toLowerCase(); 
+  		var charB = b[0].toLowerCase(); 
+  		if (charA < charB) {
+    	  return -1;
+ 		}
+		if (charA > charB) {
+		  return 1;
+		}
+
+  		return 0;
+	});
+
+	//sortiing in descending order by frequency
+	charFreqsArray.sort(function(a, b){
+		var charA = a[1] 
+  		var charB = b[1]
+  		if (charA < charB) {
+    	  return 1;
+ 		}
+		if (charA > charB) {
+		  return -1;
+		}
+		
+  		return 0;
+	});
+	return charFreqsArray;
+}
