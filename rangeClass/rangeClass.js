dd8d534d094b84337edff8a883ec9ee9fc1e883d
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-19 19:50:22 +0100

    refactor

--- rangeClass/rangeClass.js
+++ rangeClass/rangeClass.js
@@ -79,8 +79,16 @@ Range.prototype.each = function (callback = () => {}) {
 
 Range.prototype.includes = function (val) {
     if(!this.start) return null;
-
-    return (val >= this.start && val < this.end)
+    if (this.isNegative) {
+        for (let index = this.end; index >= this.start; index-= Math.abs(this.step)) {
+            if(value === index) return true;
+        }
+    } else {
+        for (let index = this.start; index <= this.end; index+= this.step) {
+            if(value === index) return true;
+        }
+    }
+    return false;
 };
 
 var range = new Range(1);
