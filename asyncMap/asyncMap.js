4bebdb307ee6051c03929b78efa8539ded818844
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-21 09:30:08 +0100

    asyncMap finnito

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -37,6 +37,36 @@
  *
  */
 
-
 var asyncMap = function(tasks, callback) {
+    var result = [];
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i](function(res) {
+            result[i] = res;
+        });
+    }
+    callback(result);
 };
+
+
+asyncMap([
+    function(cb){
+       setTimeout(function(){
+         cb('one');
+       }, 200);
+     },
+     function(cb){
+       setTimeout(function(){
+         cb('two');
+       }, 100);
+     },
+     function(cb){
+        setTimeout(function(){
+          cb('three');
+        }, 20);
+      }
+    ],
+     function(results){
+       // the results array will equal ['one','two'] even though
+       // the second function had a shorter timeout.
+       console.log(results); // ['one', 'two']
+  });
