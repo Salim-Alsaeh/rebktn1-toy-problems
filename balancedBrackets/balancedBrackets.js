eb2e72e2e4defda1fa668259f5279f0aa49655f6
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-24 09:30:24 +0100

    finsihed

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -1,6 +1,8 @@
 /*
 Balanced Brackets
-Given a string, return true if its arrangement of parenthesis (), curly-brackets {}, and square-brackets [] would be considered valid in code and mathematics (is balanced).
+Given a string, return true if its arrangement of
+parenthesis (), curly-brackets {}, and square-brackets []
+would be considered valid in code and mathematics (is balanced).
 
 isBalanced("(x + y) - (4)") // true
 isBalanced("(((10 ) ()) ((?)(:)))") // true
@@ -9,4 +11,32 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var obj = {};
+    var flag = false;
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            obj[str[i]] = 1;
+        }
+    }
+    return Object.keys(obj).every((el, i) => {
+        return isBalancedwWithType(el, str);
+    });
+    
+};
+
+var isBalancedwWithType = (type, str) => {
+    let type2 = String.fromCharCode(str.charCodeAt(0) + 2);
+    if(type === '(') type2 = ')';
+
+    let counter = 0;
+    for (var i = 0; i < str.length; i++) {
+        if(str[i] === type2 ) counter--;
+        if(counter < 0) return false;
+        if(str[i] === type ) counter++;
+    }
+    return counter === 0;
+}
+
+isBalanced("[{()}]")
+
