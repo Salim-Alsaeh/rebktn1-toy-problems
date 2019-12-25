34c855fe415859a46b0e75b4514e7c45b8e17d16
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-12-25 09:26:30 +0100

    tree implementation not finished

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,7 @@
 * rockPaperScissors(5); // => ['RRRRR', 'RRRRP', 'RRRRS', etc...]
 *
 */
-// its a tree problem
+// its a tree problem god
 var rockPaperScissors = function () {
     var chars = ['R', 'P', 'S'];
     var result = [];
@@ -30,5 +30,71 @@ var rockPaperScissors = function () {
     }
     return result;
 };
-
 rockPaperScissors()
+
+// unfinished tree
+var rockPaperScissorsTrees = (num) => {
+    var chars = ['R', 'P', 'S'];
+    var charTrees = [new Tree('R'), new Tree('P'), new Tree('S')];
+
+    var addTrees = function(node, n) {
+        if (n === 0) {
+            return;
+        }
+
+        for (var i = 0; i < charTrees.length; i++) {
+            charTrees[i].addChild(new Tree(charTrees[i]))
+        }
+    }
+
+}
+
+var Tree = function(value) {
+    var newTree = {};
+    newTree.value = value;
+    newTree.parent = null;
+    // your code here
+    newTree.children = []; 
+    extend(newTree, treeMethods)
+  
+    return newTree;
+  };
+  
+  var treeMethods = {};
+  
+  var extend = function(to, from) {
+    for (var key in from) {
+      to[key] = from[key];
+    }
+  };
+  
+  treeMethods.addChild = function(value) {
+    var node = Tree(value);
+    node.parent = this;
+    this.children.push(node)
+  };
+  
+  treeMethods.contains = function(target) {
+      var flag  = false;
+  
+      var findElement = function(element){
+        if (element.value === target ) {
+          flag = true;
+          return;
+        }
+        if (element.children) {
+           element.children.forEach(function(child){
+        findElement(child);
+           });
+        } 
+      }
+      findElement(this);
+      return flag;
+  };
+  
+  treeMethods.removeFromParent = function(){
+    this.children = [];
+    this.parent.children = this.parent.children.filter(function(element){
+      return this.value === element.value
+    });
+  }
