c158dfdbd08df7e709ce4f55937e5d7464414416
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-03 09:25:01 +0100

    tp-fin

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -1,7 +1,8 @@
 /*
 Longest Run
 
-Write a function that, given a string, finds the longest run of identical characters and returns an array containing the start and end indices of that run. 
+Write a function that, given a string, finds the longest run of identical characters
+and returns an array containing the start and end indices of that run. 
 If there are two runs of equal length, return the first one. Return [0,0] for no runs.
 
 longestRun("abbbcc") // [ 1, 3 ]
@@ -12,4 +13,27 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string) {
+    var count = 0;
+    var res = string[0];
+    if(string.length === 0) reutrn [0, 0];
+    // to get the most concecutive occurred character
+    for (var i = 0; i < string.length; i++) {
+        var currentCount = 1;
+
+        for (var j = 0; j < string.length; j++) {
+            if(string[i] === string[j])
+            currentCount++;
+        }
+
+        if(currentCount > count) {
+            count = currentCount;
+            res = string[i];
+        }
+
+    }
+    // to get the indexes of that character
+    var result = [string.indexOf(res), (string.length - 1) - string.split('').reverse().join('').indexOf(res)];
+
+    return result;
+}
