60d42544fd4f88c6bf841a0d137b5471cd976188
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2020-01-16 08:56:23 +0100

    d

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,4 +9,9 @@ primeTester(1) // false
 
 */
 
-function primeTester(n) {}
+function primeTester(n) {
+    for(var i = 2; i < n; i++) {
+        if(n % i === 0)  return false;
+    }
+    return n > 1;
+}
